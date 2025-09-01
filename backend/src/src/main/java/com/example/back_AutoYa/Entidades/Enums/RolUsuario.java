3c1ee28daa2508f3e8b03a93dade32d164e3707f
breyner <132416841+beyner62838@users.noreply.github.com>
2025-09-01 16:39:12 -0500

    Delete backend directory

--- backend/src/src/main/java/com/example/back_AutoYa/Entidades/Enums/RolUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.example.back_AutoYa.Entidades.Enums;
-
-
-public enum RolUsuario {
-    CLIENTE,
-    PROPIETARIO,
-    ADMINISTRADOR,
-    SUPER_ADMIN
-}
--- backend/src/src/main/java/com/example/back_AutoYa/Entidades/Enums/TipoTransmision.java
+++
@@ -1,9 +0,0 @@
-package com.example.back_AutoYa.Entidades.Enums;
-
-
-public enum TipoTransmision {
-    MANUAL,
-    AUTOMATICA,
-    SEMI_AUTOMATICA,
-    CVT
-}
