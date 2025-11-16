33e4bf2ce76bc243da04ad41a4ddd9fdc691ecf9
CarlosEduardoCabreraMiranda <cecabrera-2022a@corhuila.edu.co>
2025-11-16 17:04:28 -0500

    Fix(Cleaning repo): Changed connection parameters to frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:10080'
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:10080/autoya'
 
 const api = axios.create({
   baseURL,
